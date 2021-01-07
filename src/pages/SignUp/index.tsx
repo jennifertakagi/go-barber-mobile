57798148a798687feb1bfd5cd3943ad4454e1368
jennifertakagi <jennitakagi@gmail.com>
2021-01-07 10:24:19 -0300

    feat: implement user register

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,7 @@ import * as Yup from 'yup';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 
+import api from '../../services/api';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -58,13 +59,11 @@ const SignUp: React.FC = () => {
 
       await schema.validate(data, { abortEarly: false });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      addToast({
-        type: 'success',
-        title: 'Register successfully.',
-        description: 'You can login on GoBarber!',
-      });
+      Alert.alert('Register successfully!');
+
+      navigation.navigate('SignIn');
     } catch (error) {
       if (error instanceof Yup.ValidationError) {
         const errors = getValidationErrors(error);
