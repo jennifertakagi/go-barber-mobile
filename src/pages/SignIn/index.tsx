f151b270353319b1b8e00254a548b0a49cd5a641
jennifertakagi <jennitakagi@gmail.com>
2021-01-06 22:34:32 -0300

    feat: import font

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
+
+import logoImage from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImage} />
+      <Title>Logon</Title>
+    </Container>
+  );
 };
 
 export default SignIn;
