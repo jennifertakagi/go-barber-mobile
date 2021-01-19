bd139484be1e4e32bfa52cd954f22300742d5836
jennifertakagi <jennitakagi@gmail.com>
2021-01-19 14:28:45 -0300

    feat: add new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import AppointmentCreated from '../pages/AppointmentCreated';
+import CreateAppointment from '../pages/CreateAppointment';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
-const Auth = createStackNavigator();
+const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-  <Auth.Navigator
+  <App.Navigator
     screenOptions={{
       headerShown: false,
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
-    <Auth.Screen name="Dashboard" component={Dashboard} />
-  </Auth.Navigator>
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="Dashboard" component={Dashboard} />
+
+    <App.Screen name="Profile" component={Profile} />
+  </App.Navigator>
 );
 
 export default AppRoutes;
