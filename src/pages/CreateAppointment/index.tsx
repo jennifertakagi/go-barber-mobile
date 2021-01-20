83d96ba461a79f63bdd8f8d6405ccedaecb68f7f
jennifertakagi <jennitakagi@gmail.com>
2021-01-20 10:16:48 -0300

    feat: add CreateAppointment component and styles

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserAvatar,
+  BackButton,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+  const route = useRoute();
+  const { providerId } = route.params as RouteParams;
+  const { goBack } = useNavigation();
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Providers</HeaderTitle>
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
