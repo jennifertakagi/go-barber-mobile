9d4ebcd83d2277d1f5e1a3abcb6ea8388b403449
jennifertakagi <jennitakagi@gmail.com>
2021-01-20 20:42:45 -0300

    feat: createq AppointmentCreate component and styleQ

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>The appointment was created</Title>
+      <Description>Terça, dia 14 as blabla</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
