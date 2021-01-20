54c3994a537529351d59f79ad9fe15da9cd2021c
jennifertakagi <jennitakagi@gmail.com>
2021-01-20 15:06:00 -0300

    feat: add behavior to change providers

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList } from 'react-native';
+import { Provider } from './index';
+import { RectButton } from 'react-native-gesture-handler';
+
+interface ProviderContainerProps {
+  selected: boolean;
+}
+
+interface ProviderNameProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -30,3 +41,33 @@ export const UserAvatar = styled.Image`
 `;
 
 export const BackButton = styled.TouchableOpacity``;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled.(RectButton)<ProviderContainerProps>`
+  background: ${props => (props.selected ? '#3b4b47' : '#3e3b47')};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
+`;
